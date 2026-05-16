7d5faadc2ec8e91944b0fe6814267e7864e9b201
ali <alisalahi@guep.com.br>
2026-05-16 09:11:54 -0300

    feat(storefront): add CheckoutService (preview + place-order)

---
+++ frontend/storefront/src/app/core/services/checkout.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import {
+  CheckoutPreviewRequest,
+  CheckoutPreviewResponse,
+  PlaceOrderRequest,
+  PlaceOrderResponse,
+} from '../models/checkout';
+
+@Injectable({ providedIn: 'root' })
+export class CheckoutService {
+  private readonly http = inject(HttpClient);
+  private readonly api = environment.apiBase;
+
+  preview(req: CheckoutPreviewRequest): Observable<CheckoutPreviewResponse> {
+    return this.http.post<CheckoutPreviewResponse>(`${this.api}/checkout/preview`, req);
+  }
+
+  placeOrder(req: PlaceOrderRequest): Observable<PlaceOrderResponse> {
+    return this.http.post<PlaceOrderResponse>(`${this.api}/checkout/place-order`, req);
+  }
+}
